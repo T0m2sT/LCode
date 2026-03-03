f350b571047dc989f300451ee73dd46e21c40f74
Pinho <rafaelpinhotsilva@gmail.com>
2026-03-03 22:14:49 +0000

    Bitwise operations finished

--- lab1/bitwise.c
+++ lab1/bitwise.c
@@ -2,14 +2,45 @@
 
 #define TODO return 255
 
-uint8_t clear(uint8_t msk, int pos) { TODO; }
+uint8_t clear(uint8_t msk, int pos) {
+  uint8_t clear_mask = (uint8_t) ~(1u << pos);
 
-uint8_t set(uint8_t msk, int pos) { TODO; }
+  return msk & clear_mask;
+}
 
-bool is_set(uint8_t msk, int pos) { TODO; }
+uint8_t set(uint8_t msk, int pos) {
+  uint8_t set_mask = (uint8_t) (1u << pos);
 
-uint8_t lsb(uint16_t wide_msk) { TODO; }
+  return msk | set_mask;
+}
 
-uint8_t msb(uint16_t wide_msk) { TODO; }
+bool is_set(uint8_t msk, int pos) {
+  uint8_t is_set_mask = (uint8_t) (1u << pos);
 
-uint8_t mask(int pos, ...) { TODO; }
+  return (msk & is_set_mask) != 0;
+}
+
+uint8_t lsb(uint16_t wide_msk) {
+  return (uint8_t) wide_msk;
+}
+
+uint8_t msb(uint16_t wide_msk) {
+  return (uint8_t) (wide_msk >> 8);
+}
+
+uint8_t mask(int pos, ...) {
+  uint8_t msk = 0; 
+  va_list args;
+  va_start(args, pos);
+
+  while (pos != MSK_END) {
+    if (pos >= 0 && pos < 8) {
+     msk |= (uint8_t) (1u << pos); 
+    }
+
+    pos  = va_arg(args, int);
+  }
+
+  va_end(args);
+  return msk;
+}
--- lab1/bitwise.h
+++ lab1/bitwise.h
@@ -2,6 +2,7 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdarg.h>
 
 #define MSK_END -1
 
