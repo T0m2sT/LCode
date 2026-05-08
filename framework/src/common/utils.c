5ce8db92367a43ec655366034cf99c71336bd389
Pinho <rafaelpinhotsilva@gmail.com>
2026-05-08 21:56:20 +0100

    Added util_sys_inb

---
+++ framework/src/common/utils.c
@@ -0,0 +1,16 @@
+#include <lcom/lcf.h>
+
+#include <stdint.h>
+
+// Reads value from port
+int (util_sys_inb)(int port, uint8_t *value) {
+  
+  uint32_t temp;
+
+  if (sys_inb(port, &temp) != OK)
+    return 1;
+  
+  *value = (uint8_t) temp;
+
+  return 0;
+}
