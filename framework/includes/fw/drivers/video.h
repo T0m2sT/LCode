2505818a20b5fc9faa7017cec832485f9ff78ee7
Pinho <rafaelpinhotsilva@gmail.com>
2026-06-03 01:55:11 +0100

    Added backbuffer to framework

--- framework/includes/fw/drivers/video.h
+++ framework/includes/fw/drivers/video.h
@@ -3,20 +3,27 @@
 #include <lcom/lcf.h>
 
 
+unsigned vg_get_h_res();
+unsigned vg_get_v_res();
+
 int set_graphics_mode(uint16_t mode);
 int set_text_mode();
 
 int vg_map_vram(uint16_t mode);
-
-// Getters for projects buffer
-void*    vg_get_video_mem();
-unsigned vg_get_h_res();
-unsigned vg_get_v_res();
-unsigned vg_get_bytes_per_pixel();
-unsigned vg_get_frame_size();
 
 int (vg_draw_pixel)(uint16_t x, uint16_t y, uint32_t color);
 int (vg_draw_hline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color);
 int (vg_draw_rectangle)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
 
 int (print_xpm)(xpm_map_t xpm, uint16_t x, uint16_t y);
+
+int video_init(uint16_t mode);
+void video_cleanup();
+void vg_flip_buffer();
+void vg_flip_region(uint16_t x, uint16_t y, uint16_t w, uint16_t h);
+
+void bb_draw_pixel(uint16_t x, uint16_t y, uint32_t color);
+uint32_t bb_get_pixel(uint16_t x, uint16_t y);
+void bb_draw_hline(uint16_t x, uint16_t y, uint16_t len, uint32_t color);
+void bb_draw_rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint32_t color);
+void bb_clear(uint32_t color);
