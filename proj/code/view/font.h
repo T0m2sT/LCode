57f96172f4cebdd54287238afb3213bcfd4d34d2
Miguel Rocha <miguelmirandarocha@gmail.com>
2026-06-04 18:46:40 +0100

    View documentation - font

--- proj/code/view/font.h
+++ proj/code/view/font.h
@@ -2,8 +2,28 @@
 
 #include <lcom/lcf.h>
 
-#define FONT_W 8
-#define FONT_H 16
+/**
+ * @file font.h
+ * @brief Bitmap font rendering (8x16 pixels per char).
+ */
 
+#define FONT_W 8 /**< Letter's width in pixels */
+#define FONT_H 16 /**< Letter's height in pixels */
+
+/**
+ * @brief Draws a single ASCII character into the back buffer
+ * @param x Pixel x coordinate of the glyph's top-left corner
+ * @param y Pixel y coordinate of the glyph's top-left corner
+ * @param c Character to draw. Ignored if outside the ASCII range (>= 128)
+ * @param color 24-bit RGB color for the foreground pixels
+ */
 void draw_char(int x, int y, char c, uint32_t color);
+
+/**
+ * @brief Draws a null-terminated string into the back buffer
+ * @param x Pixel x coordinate of the first glyph's top-left corner
+ * @param y Pixel y coordinate of the string baseline
+ * @param s Null-terminated string to draw
+ * @param color 24-bit RGB color for the foreground pixels
+ */
 void draw_string(int x, int y, const char *s, uint32_t color);
